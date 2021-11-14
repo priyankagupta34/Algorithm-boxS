488b69f9ddb95084c397b16c186e5b8355864637
priyankagupta34 <34priyankagupta@gmail.com>
2021-11-14 21:00:22 +0530

    jty

--- src/js/AlgoImgModel.js
+++ src/js/AlgoImgModel.js
@@ -6,6 +6,7 @@ export default function AlgoImgModel() {
 
   const model_src = useSelector((state) => state.model_src);
   const model_open = useSelector((state) => state.model_open);
+  const rotate_img = useSelector((state) => state.rotate_img);
 
   if (!model_open) return null;
   return ReactDOM.createPortal(
@@ -19,11 +20,31 @@ export default function AlgoImgModel() {
       <div className="vimodel">
         <img
           className="imgM"
+          style={{
+            transform: `rotate(${rotate_img}deg)`,
+            width: `${rotate_img === "90" ? "89vh" : "96vw"}`
+          }}
           src={model_src}
           alt="Algo"
           onClick={(e) => e.stopPropagation()}
         />
-        <div className="cgh">Click Anywhere (other than image) to close</div>
+        <div className="cgh">
+          <div className="flex">
+            <div>Click Anywhere (other than image) to close</div>
+            <span
+              className="degf"
+              onClick={(e) => {
+                e.stopPropagation();
+                dispatch({ type: "ROTATE_IMAGE" });
+              }}
+              style={{
+                transform: `rotate(${rotate_img === "0" ? "90" : "0"}deg)`
+              }}
+            >
+              &#8634;
+            </span>
+          </div>
+        </div>
       </div>
     </div>,
     document.body
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -2,7 +2,8 @@ const initialState = {
   search: "",
   allDefs: [],
   model_open: false,
-  model_src: ""
+  model_src: "",
+  rotate_img: "0"
 };
 
 export default function reducer(prevState = initialState, { type, payload }) {
@@ -21,7 +22,13 @@ export default function reducer(prevState = initialState, { type, payload }) {
       return {
         ...prevState,
         model_open: !prevState.model_open,
-        model_src: payload
+        model_src: payload,
+        rotate_img: "0"
+      };
+    case "ROTATE_IMAGE":
+      return {
+        ...prevState,
+        rotate_img: prevState.rotate_img === "0" ? "90" : "0"
       };
     default:
       return { ...prevState };
